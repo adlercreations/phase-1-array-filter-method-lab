31319c899d09c5c09eaced6a39fc8bdfbe5093b6
Stephen Adler <adlercreations@gmail.com>
2024-07-20 17:31:45 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+function findMatching(drivers, nameToMatch) {
+    return drivers.filter(driverName => {
+        return driverName.toUpperCase() === nameToMatch.toUpperCase();
+    })
+}
+
+function fuzzyMatch(driverNames, startsWithString) {
+    return driverNames.filter(driverName => {
+        return driverName.startsWith(startsWithString);
+    })
+}
+
+function matchName(drivers, name){
+    return drivers.filter(driver => {
+        return driver.name === name;
+        })
+}
+
+function myFilter(arrayOfItems, callBackFunc) {
+    const filteredItemsToReturn = [];
+
+    for(item of arrayOfItems){
+        if(callBackFunc(item) === true) {
+            filteredItemsToReturn.push(item);
+        };
+    }
+
+    return filteredItemsToReturn;
+}
+
+const names = ["Alice", "Alex", "Bob"];
+const filterNames = myFilter(names, (item) => {
+    return item.startsWith("Al")
+});
